cecbedb0b3ca2bc390794c513bee8eb1c41f1c16
陈凯旋 <chenkaixuan@doctortech.com.cn>
2022-01-20 15:48:34 +0800

    添加接口注释

--- src/types/router.d.ts
+++ src/types/router.d.ts
@@ -1,11 +1,10 @@
 import {  LazyExoticComponent } from 'React'
 
 export interface RouteInterface {
-  path: string
-  index?: boolean,
-  to?: string,
-  component?: any
-  redeact?: any,
-  children?: Array<Route> | null,
-  meta?: any
+  path: string  // 路由路径
+  index?: boolean // 是否是默认路由
+  to?: string  // 重定向路径，需要引入 /components/Redirect 组件
+  component?: LazyExoticComponent<() => JSX.Element> | JSX.Element | any // 懒加载组件
+  children?: Array<RouteInterface> | null // 子路由
+  meta?: any // 其他路由信息
 }
