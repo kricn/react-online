2eb2d796940c3d41c8f114aeeaa0cd613ff877c1
陈凯旋 <chenkaixuan@doctortech.com.cn>
2022-01-25 14:06:51 +0800

    侧边栏收缩

--- src/layout/default.tsx
+++ src/layout/default.tsx
@@ -1,19 +1,34 @@
+// 第三方
 import { Outlet } from 'react-router-dom'
 import { Layout } from 'antd';
+import { useState } from 'react';
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+} from '@ant-design/icons'
 
+// 自定义
 import SiderNav from './components/SiderNav'
 
-const style = require('./default.module.scss');
+// 样式
+const style = require('./default.module.scss').default;
 
 const { Sider, Header, Content } = Layout
 
 function DefaultLayout () {
+  const [collapsed, setcollapsed] = useState(false)
+
+  const toggleCollapsed = () => {
+    setcollapsed(!collapsed)
+  }
+
   return (
     <Layout>
-      <Sider collapsible
-        trigger={null}
-      >
+      <Sider collapsible trigger={null} collapsed={collapsed} >
         <SiderNav />
+        <div className={style.trigger} onClick={() => toggleCollapsed ()}>
+          { collapsed ? <MenuFoldOutlined /> : <MenuUnfoldOutlined /> }
+        </div>
       </Sider>
       <Layout>
         <Header style={{background: '#fff', padding: '0 16px'}}>
