d9f504768033cd6e9bc12d8061c53d805971adca
陈凯旋 <chenkaixuan@doctortech.com.cn>
2022-01-25 14:07:12 +0800

    删除无用代码

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,7 +1,6 @@
 import { Button } from 'antd'
 
 const style = require('./index.module.scss').default
-console.log(style)
 export default function Home() {
   return (
     <div className={style.home}>
