7e9aa824d87b4a941a9d0cdc0e4ef36df5bf6de6
陈凯旋 <chenkaixuan@doctortech.com.cn>
2022-01-25 14:05:58 +0800

    侧边栏icon & 侧边栏展开调整

--- src/layout/components/SiderNav/index.tsx
+++ src/layout/components/SiderNav/index.tsx
@@ -14,17 +14,30 @@ function SiderNav() {
   const navigate = useNavigate()
   const location = useLocation()
 
-  const renderMenu = (routes:Array<RouteInterface>)=> {
+  // 获取默认打开的子菜单
+  const getOpenKey = (path: string): Array<string> => {
+    let result: Array<string> = []
+    const pathArray = path.split('/')
+    for (let i = 2; i < pathArray.length; i ++) {
+      result.push(pathArray.slice(0, i).join('/'))
+    }
+    return result
+  }
+
+  // 渲染侧边栏
+  const renderMenu = (routes:Array<RouteInterface>) => {
     return (
       <Menu 
         theme="dark" 
         mode="inline"
         defaultSelectedKeys={[location.pathname]}
+        defaultOpenKeys={getOpenKey(location.pathname)}
       >
         {
           routes.map(route => {
             if (!route.children) return renderFirstMenu(route)
             if (route.children.length === 1) return renderFirstMenu(route.children[0])
+            // 递归多级菜单
             if (route.children.length > 1) return renderMultMenu(route)
             return ''
           })
@@ -33,6 +46,7 @@ function SiderNav() {
     )
   }
   
+  // 渲染一级菜单
   const renderFirstMenu = (route: RouteInterface) => {
   
     const toPage = (path:string) => {
@@ -40,13 +54,14 @@ function SiderNav() {
     }
   
     return (
-      !route?.meta?.hidden ? <Menu.Item key={route.path} onClick={() => toPage(route.path)}>{route?.meta?.title}</Menu.Item> : ''
+      !route?.meta?.hidden ? <Menu.Item icon={route.meta?.icon ? <route.meta.icon /> : ''} key={route.path} onClick={() => toPage(route.path)}>{route?.meta?.title}</Menu.Item> : ''
     )
   }
   
+  // 渲染多级惨淡
   const renderMultMenu = (subRoute: RouteInterface) => {
     return (
-      <SubMenu title={subRoute.meta.title} key={subRoute.path}>
+      <SubMenu title={subRoute.meta.title} key={subRoute.path} icon={subRoute.meta?.icon ? <subRoute.meta.icon /> : ''}>
         {
           subRoute.children?.map(item => {
             return renderFirstMenu(item)
