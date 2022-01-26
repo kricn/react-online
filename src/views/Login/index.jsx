cd4e8981f8774b091239a7f358cb0ba21f4326f6
陈凯旋 <chenkaixuan@doctortech.com.cn>
2022-01-26 17:26:16 +0800

    登录页面

--- src/views/Login/index.jsx
+++ src/views/Login/index.jsx
@@ -1,3 +1,12 @@
-export default function Login() {
-  return <div>login</div>;
+
+const style = require('./index.module.scss').default
+
+function Login() {
+  return (
+    <div className={style.loginWrapper}>
+      aaa
+    </div>
+  )
 }
+
+export default Login
