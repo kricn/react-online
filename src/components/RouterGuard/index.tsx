651f16bfffa3a2b4b4dbfd6d836298aac69713bf
陈凯旋 <chenkaixuan@doctortech.com.cn>
2022-01-26 15:25:24 +0800

    路由守卫调整

--- src/components/RouterGuard/index.tsx
+++ src/components/RouterGuard/index.tsx
@@ -1,52 +1,73 @@
-import { Suspense } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import { Routes, Route, Navigate } from 'react-router';
+import { inject, observer } from 'mobx-react'
 
 import LazyLoading from '@/components/UnitComponent/LazyLoading';
 
 import routers from '@/router/index'
-import { generateRoute } from './helper'
 import { RouteInterface } from '@/types/router'
+import { deepCopy } from '@/utils';
+// import { useAsyncState } from '../UseAsyncState';
 
-const useElement:any = (route:RouteInterface) => {
-  return (
-    (
-      <Suspense fallback={<LazyLoading />}>
-        <route.component {...route} />
-      </Suspense>
-    ) 
-  )
+
+const generateRoute:any = (routes:Array<RouteInterface>, isLogin: boolean = false, userInfo: any = {}) => {
+  const temp: Array<RouteInterface> = deepCopy(routes)
+  return temp.filter(item => {
+    if (item.children?.length) item.children = generateRoute(item.children, isLogin)
+    const { meta } = item
+    return meta?.auth && !isLogin ? false : true
+  })
 }
 
-// 只渲染对应的Route,外层需要用Routes包裹
-const renderRoute:any = (routes:Array<RouteInterface>) => {
-  return (
-    routes.map(item => {
-      return (
-        <Route
-          {...item}
-          key={item.path}
-          path={item.path}
-          element={
-            useElement(item)
-          }
-        >
-          {/* 嵌套路由 */}
-          {item.children?.length ? renderRoute(item.children) : ''}
-        </Route>
-      )
-    })
-  )
-}
+function RouteView ( {appStore}: any) {
 
-function RouteView () {
+  const [isLogin, setIsLogin] = useState(appStore.isLogin)
+
+  useEffect(() => {
+    setIsLogin(appStore.isLogin)
+    console.log(isLogin)
+  }, [appStore.isLogin, isLogin])
+
+  // 渲染路由对应的组件
+  const renderElement:any = (route:RouteInterface) => {
+    return (
+      (
+        <Suspense fallback={<LazyLoading />}>
+          <route.component {...route} />
+        </Suspense>
+      ) 
+    )
+  }
+  
+  // 只渲染对应的Route,外层需要用Routes包裹
+  const renderRoute:any = (routes:Array<RouteInterface>) => {
+    return (
+      routes.map(item => {
+        return (
+          <Route
+            {...item}
+            key={item.path}
+            path={item.path}
+            element={
+              renderElement(item)
+            }
+          >
+            {/* 嵌套路由 */}
+            {item.children?.length ? renderRoute(item.children) : ''}
+          </Route>
+        )
+      })
+    )
+  }
+
   return (
     <Routes>
       {
-        renderRoute(generateRoute(routers))
+        renderRoute(generateRoute(routers, isLogin))
       }
       <Route path="*" element={<Navigate to="/login" />} />
     </Routes>
   )
 }
 
-export default RouteView;
+export default inject("appStore")(observer(RouteView));
