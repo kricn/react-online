b4bde690290f01621985c09852f48449ae465618
kricn <mnyt2436@126.com>
2022-01-28 01:04:08 +0800

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { inject, observer } from 'mobx-react'
+
 import RouteView from '@/components/RouterGuard';
 import './App.css';
 
-function App() {
+function App({appStore}: any) {
+
+  // 登录状态改变在这里处理
+  console.log(appStore.isLogin)
+
   return <RouteView />
 }
 
-export default App;
+export default inject('appStore')(observer(App));
