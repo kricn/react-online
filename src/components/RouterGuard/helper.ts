a4578397c43a774605231e1893ed1eaf64e1f10b
陈凯旋 <chenkaixuan@doctortech.com.cn>
2022-01-27 16:23:18 +0800

    添加注释

--- src/components/RouterGuard/helper.ts
+++ src/components/RouterGuard/helper.ts
@@ -4,8 +4,11 @@ import { RouteInterface } from '@/types/router'
 import { deepCopy } from '@/utils';
 
 const generateRoute:any = (routes:Array<RouteInterface>, isLogin: boolean = false, userInfo: any = {}) => {
+  // 深拷贝一次路由数组
+  // 不然在压栈过程中会丢失原来的路由子项
   const temp: Array<RouteInterface> = deepCopy(routes)
   return temp.filter(item => {
+    // 压栈
     if (item.children?.length) item.children = generateRoute(item.children, isLogin)
     const { meta } = item
     return meta?.auth && !isLogin ? false : true
