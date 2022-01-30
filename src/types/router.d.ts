b1c1fa8ef10bc08ce23c776c476c76e9c72f020c
kricn <mnyt2436@126.com>
2022-01-30 21:06:06 +0800

    类型调整

--- src/types/router.d.ts
+++ src/types/router.d.ts
@@ -1,10 +1,17 @@
 import {  LazyExoticComponent } from 'React'
 
+interface MetaInterface {
+  title?: string
+  hidden?: boolean
+  icon?: any
+  auth?: boolean
+}
+
 export interface RouteInterface {
   path: string // 路由路径
   index?: boolean // 是否是默认路由
   to?: string  // 重定向路径，需要引入 /components/Redirect 组件
   component?: LazyExoticComponent<() => JSX.Element> | JSX.Element | any // 懒加载组件
   children?: Array<RouteInterface> // 子路由
-  meta?: any // 其他路由信息
+  meta?:  MetaInterface // 其他路由信息
 }
