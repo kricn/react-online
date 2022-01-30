6b1347e366ba2b89107e90213c3338f7a3a67e18
kricn <mnyt2436@126.com>
2022-01-30 21:29:13 +0800

    更改路由过滤逻辑

--- src/components/RouterGuard/helper.ts
+++ src/components/RouterGuard/helper.ts
@@ -1,17 +1,27 @@
 
 import { RouteInterface } from '@/types/router'
 
-import { deepCopy } from '@/utils';
-
 const generateRoute:any = (routes:Array<RouteInterface>, isLogin: boolean = false, userInfo: any = {}) => {
   // 深拷贝一次路由数组
   // 不然在压栈过程中会丢失原来的路由子项
-  const temp: Array<RouteInterface> = deepCopy(routes)
-  return temp.filter(item => {
-    // 压栈
-    if (item.children?.length) item.children = generateRoute(item.children, isLogin)
-    const { meta } = item
-    return meta?.auth && !isLogin ? false : true
+  // const temp: Array<RouteInterface> = deepCopy(routes)
+  // return routes.filter(item => {
+  //   // 压栈
+  //   if (item.children?.length) item.children = generateRoute(item.children, isLogin)
+  //   const { meta } = item
+  //   return meta?.auth && !isLogin ? false : true
+  // })
+  /**
+   * 深拷贝后，会导致子路由跳转时，父路由的组件没有更新也会被重新渲染，这样就会导致组件的状态被重置
+   */
+
+  return routes.map(item => {
+    if (item?.children?.length) item.children = generateRoute(item.children, isLogin)
+    if (!item.meta) item.meta = {}
+    if (item?.meta?.auth && !isLogin) {
+      item.meta.hidden = true
+    }
+    return item
   })
 }
 
