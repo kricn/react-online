8907663ae24a3cb62e278082088d01ddf0eb3c1d
kricn <mnyt2436@126.com>
2022-02-03 18:54:34 +0800

    添加退出逻辑

--- src/layout/components/HeaderBar/index.tsx
+++ src/layout/components/HeaderBar/index.tsx
@@ -1,33 +1,35 @@
 
 import { inject, observer } from 'mobx-react'
-import { useEffect } from 'react';
+import { Menu, Dropdown } from 'antd';
 // import { useAsyncState } from '@/components/UseAsyncState';
 
+import { removeToken } from '@/utils/session'
 const style = require('./index.module.scss').default
 
 function HeaderBar({ appStore }: any) {
 
-  useEffect(() => {
-    // let timer: NodeJS.Timeout = setTimeout(() => {
-    //   appStore.toggleLogin(false)
-    // }, 2000)
+  const logout = () => {
+    appStore.toggleLogin(false)
+    removeToken()
+  }
 
-    // let timer2:any = setTimeout(() => {
-    //   appStore.toggleLogin(false)
-    // }, 5000);
+  const menu = (
+    <Menu>
+      <Menu.Item key="out" onClick={logout}>退出登录</Menu.Item>
+    </Menu>
+  );
 
-    return () => {
-      // timer && clearTimeout(timer)
-      // clearTimeout(timer2)
-    }
-  }, [appStore])
   return (
     <div className={`flex fvertical ${style.header}`}>
-      <div className={style.username}>用户名{appStore.isLogin ? '已登录' : '未登录'}</div>
-      {/* <div className='note'></div> */}
-      <div className={`${style.avatar} mgl_20 pointer`}>
-        <img src="https://p1-jj.byteimg.com/tos-cn-i-t2oaga2asx/gold-user-assets/2019/11/26/16ea83b0a0fe25a6~tplv-t2oaga2asx-no-mark:100:100:100:100.awebp" alt="avatar" />
-      </div>
+      <Dropdown overlay={menu}>
+        <div className='flex fvertical pointer'>
+          <div className={style.username}>用户名{appStore.isLogin ? '已登录' : '未登录'}</div>
+          {/* <div className='note'></div> */}
+          <div className={`${style.avatar} mgl_20`}>
+            <img src="https://p1-jj.byteimg.com/tos-cn-i-t2oaga2asx/gold-user-assets/2019/11/26/16ea83b0a0fe25a6~tplv-t2oaga2asx-no-mark:100:100:100:100.awebp" alt="avatar" />
+          </div>
+        </div>
+      </Dropdown>
     </div>
   );
 }
