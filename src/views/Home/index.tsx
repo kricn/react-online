54040c3035a2f72be6564864a0ba7b6edfaa1140
kricn <mnyt2436@126.com>
2022-02-05 01:10:45 +0800

    文件名调整

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,11 +1,9 @@
-import { Button } from 'antd'
 
 const style = require('./index.module.scss').default
 export default function Home() {
   return (
     <div className={style.home}>
       home
-      <Button type="primary">Home Click</Button>
     </div>
   )
 }
