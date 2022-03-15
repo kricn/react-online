5ca91c1128e1381aab89068d64761f7b468cfd63
陈凯旋 <chenkaixuan@doctortech.com.cn>
2022-03-15 11:24:45 +0800

    测试接口

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,9 +1,34 @@
+import { testRequest } from '@/api/index'
+import { useEffect, useState } from 'react'
 
 const style = require('./index.module.scss').default
 export default function Home() {
+
+  const [list, setList] = useState<any[]>([])
+  const [loading, setLoading] = useState<boolean>(false)
+
+  const getList = async () => {
+    setLoading(true)
+    const res = await testRequest()
+    if (res.code === 1) {
+      setList(res?.data)
+    }
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    getList()
+  }, [])
+
   return (
     <div className={style.home}>
       home
+      {
+        loading ? 'loading.....' :
+        list.map(item => {
+          return <div key={item.value}>{item.label} - {item.value}</div>
+        })
+      }
     </div>
   )
 }
