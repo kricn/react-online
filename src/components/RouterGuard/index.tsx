41d325e1cdaa10d78e75b63262d34ac61f9a2fe6
kricn <mnyt2436@126.com>
2022-03-25 20:42:09 +0800

    修复JSON.parse报错

--- src/components/RouterGuard/index.tsx
+++ src/components/RouterGuard/index.tsx
@@ -17,7 +17,7 @@ function RouteView ( {appStore}: any) {
 
   // 判断是否已经登录过
   useEffect(() => {
-    const userInfo = JSON.parse(getToken('user-info'))
+    const userInfo = getToken('user-info') && JSON.parse(getToken('user-info'))
     userInfo.username ? appStore.toggleLogin(userInfo) : appStore.toggleLogin({})
     setLoading(false)
   }, [loading, appStore])
