c4129dfc1aeecea418386682b7d0d6066abff55e
陈凯旋 <chenkaixuan@doctortech.com.cn>
2022-04-13 17:25:53 +0800

    增加测试页面

--- src/views/Test/index.tsx
+++ src/views/Test/index.tsx
@@ -1,5 +1,47 @@
+import { useAsyncState } from '@/components/UseAsyncState';
+import { useCallback, useEffect, useRef } from 'react'
+
+interface List {
+  label: string
+  value: number
+}
+
 export default function Test() {
+
+  const [list, setList] = useAsyncState<List[]>([]);
+
+  const unmountedRef = useRef<boolean>(false);
+
+  const getList = useCallback(async () => {
+    await new Promise(resolve => setTimeout(resolve, 1000))
+    const tmp = []
+    for (let i = 0; i < 10; i ++) {
+      tmp.push({
+        label: `第 ${i + 1} 项`,
+        value: i + 1
+      })
+    }
+    setList(tmp)
+  }, [setList])
+
+  useEffect(() => {
+    return () => {
+      unmountedRef.current = true
+    }
+  }, [])
+
+  useEffect(() => {
+    getList()
+  }, [getList])
+
   return (
-    <div>Test</div>
+    <div>
+      <h1>测试页面</h1>
+      {
+        list.map(item => {
+          return <div key={item.value}>{item.label}</div>
+        })
+      }
+    </div>
   )
 }
