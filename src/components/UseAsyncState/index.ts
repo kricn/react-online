9f702db4f4668007357bae69aefdf2b77efbe107
陈凯旋 <chenkaixuan@doctortech.com.cn>
2022-04-13 17:25:45 +0800

    自定义 hook, 统一在卸载时阻止设置数据

--- src/components/UseAsyncState/index.ts
+++ src/components/UseAsyncState/index.ts
@@ -1,28 +1,20 @@
 import { Dispatch, SetStateAction, useCallback, useEffect, useRef, useState } from 'react';
 
 /**
- * useUnmounted
- * @returns boolean
- * whether the component is unmounted
- */
-export function useUnmounted() {
-  const unmountedRef = useRef(false);
-  useEffect(() => {
-    return () => {
-      unmountedRef.current = true;
-    };
-  }, []);
-  return unmountedRef.current;
-}
-/**
  * @method useAsyncState
  */
-export function useAsyncState<S>(initialState?: S | (() => S)): [S | undefined, Dispatch<SetStateAction<S>>] {
-  const unmountedRef = useUnmounted();
+export function useAsyncState<S>(initialState: S | (() => S)): [S, Dispatch<SetStateAction<S>>] {
+  const unmountedRef = useRef<boolean>(false);
   const [state, setState] = useState(initialState);
   const setAsyncState = useCallback((s) => {
-    if (unmountedRef) return;
+    if (unmountedRef.current) return;
     setState(s);
   }, []);
+
+  useEffect(() => {
+    return () => {
+      unmountedRef.current = true
+    }
+  }, [])
   return [state, setAsyncState];
 }
