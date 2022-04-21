20d33634ec086d32552af06381677a0651a21b0f
陈凯旋 <chenkaixuan@doctortech.com.cn>
2022-04-21 17:38:07 +0800

    动画组件完善

--- src/views/Animation/index.tsx
+++ src/views/Animation/index.tsx
@@ -1,26 +1,31 @@
 
-import TransitionWrapper from './components/TransitionWrapper'
+// import TransitionWrapper from './components/TransitionWrapper'
+import { useState } from 'react'
+import { SwitchTransition, CSSTransition } from 'react-transition-group'
+import { Button } from 'antd'
 import style from './index.module.scss'
 
 export default function Anaimation() {
+  const [trigger, setTrigger] = useState<boolean>(false)
+
+  const toggleBoxStatus = () => {
+    setTrigger(val => !val)
+  }
 
   return (
-    <div className={style.container}>
-      {/* <SwitchTransition mode="out-in">
-        <CSSTransition
-          classNames={`pointer ${isHover ? 'fade-sildeX' : ''}`}
-          timeout={300}
-          key={isHover ? 'in' : 'out'}
-          appear
-        >
-          <div className={style.box} onMouseEnter={toggleBoxStatus} onMouseOut={toggleBoxStatus}></div>
-        </CSSTransition>
-      </SwitchTransition> */}
-      <TransitionWrapper
-        classNames='fade-sildeX'
-      >
-        <div className={`${style.box}`}></div>
-      </TransitionWrapper>
+    <div>
+      <Button onClick={toggleBoxStatus}>trigger</Button>
+        <SwitchTransition mode="out-in">
+          <CSSTransition 
+            classNames={`slide-scale`}
+            timeout={300}
+            key={trigger ? "in" : "off"
+          }>
+            <div style={{width: '100px', height: '100px', background: '#1818'}}>
+              aaa
+            </div>
+          </CSSTransition>
+        </SwitchTransition>
     </div>
   )
 }
