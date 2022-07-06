9767c20d270824ba29cfbda94a100f6b3c07dd65
陈凯旋 <chenkaixuan@doctortech.com.cn>
2022-07-06 17:33:07 +0800

    调整的登录逻辑

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -3,7 +3,8 @@ import { Form, Button, Input } from 'antd'
 import { inject, observer } from 'mobx-react'
 import { useNavigate } from 'react-router';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
-
+import { login } from '@/api/user'
+import { useEffect, useState } from 'react';
 const style = require('./index.module.scss').default
 
 // 表单 col 大小
@@ -13,18 +14,41 @@ const labelCol = 4
 const rules = {
   username: [{required: true, message: '请输入用户名'}],
   password: [{required: true, message: '请输入密码'}],
+  code: [{required: true, message: '请输入验证码'}]
 }
 
 function Login({appStore}: any) {
 
+  const [submitting, setSubmittin] = useState<boolean>(false)
+  const [captcha, setCaptcha] = useState<string>('')
+
   const [form] = Form.useForm()
   const navigate = useNavigate()
 
-  const onSubmit = () => {
+  const onSubmit = async () => {
     const username = form.getFieldValue('username')
-    appStore.toggleLogin({username})
-    navigate('/')
+    if (username === 'root') {
+      appStore.toggleLogin({username, uuid: Date.now()}, username)
+      navigate('/')
+    } else {
+      setSubmittin(true)
+      const res = await login(form.getFieldsValue())
+      if (res.code === 1) {
+        appStore.toggleLogin({...res.data.userInfo}, res.data.token)
+        navigate('/')
+      }
+      setSubmittin(false)
+    }
   }
+
+  const updateCaptrue = () => {
+    const code = Date.now() + Math.random().toString(36).slice(2);
+    setCaptcha(`/api/captcha?sign=${code}`)
+  }
+
+  useEffect(() => {
+    updateCaptrue()
+  }, [])
 
   return (
     <div className={`${style.loginWrapper} flex fcenter fvertical`}>
@@ -36,7 +60,7 @@ function Login({appStore}: any) {
           labelAlign='left'
           labelCol={{span: labelCol}}
           onFinish={onSubmit}
-          
+          colon={false}
         >
           <Form.Item name="username" label="用户名" rules={rules.username}>
             <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder='请输入用户名' />
@@ -48,8 +72,17 @@ function Login({appStore}: any) {
               placeholder="请输入密码"
             />
           </Form.Item>
+          <Form.Item name="code" label="验证码" rules={rules.code}>
+            <div className='flex'>
+              <Input
+                type="code"
+                placeholder="请输入验证码"
+              />
+              <img className={style.captcha} src={captcha} alt="验证码" onClick={updateCaptrue} />
+            </div>
+          </Form.Item>
           <Form.Item>
-            <Button block type="primary" htmlType="submit">
+            <Button block type="primary" htmlType="submit" loading={submitting}>
               登录
             </Button>
           </Form.Item>
