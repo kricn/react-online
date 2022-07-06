09e69cff5ec69889c98a71b84791681d116e52ba
陈凯旋 <chenkaixuan@doctortech.com.cn>
2022-07-06 17:32:16 +0800

    调整类型接口位置

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -3,3 +3,10 @@
 declare module 'jspdf'
 
 declare module 'html2canvas'
+
+// 请求体返回结构
+interface ResponseBody {
+  code: number
+  msg?: string,
+  data?: any
+}
--- src/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-
-// 请求体返回结构
-export interface ResponseBody {
-  code: number
-  msg?: string,
-  data?: any
-}
