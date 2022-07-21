a732c9df85aaa4af48a51e1fbd8af639ca3304ac
陈凯旋 <chenkaixuan@doctortech.com.cn>
2022-07-21 16:25:11 +0800

    调整函数位置

--- src/components/RouterGuard/index.tsx
+++ src/components/RouterGuard/index.tsx
@@ -12,8 +12,45 @@ import { getToken } from '@/utils/session'
 import { getUserInfo } from '@/api/user';
 import { useAsyncState } from '../UseAsyncState';
 
+// 渲染路由对应的组件
+const renderElement = (route:RouteInterface):any => {
+  return (
+    // 不支持渲染的，则跳转到对应配置的路径或404
+    !route?.meta?.noRender ?
+    (
+      <Suspense fallback={<LazyLoading />}>
+        <route.component {...route} />
+      </Suspense>
+    ) : <Navigate to={route?.to || '/404'} />
+  )
+}
 
-function RouteView ( {appStore}: any) {
+// 只渲染对应的Route,外层需要用Routes包裹
+const renderRoute:any = (routes:Array<RouteInterface>) => {
+  return (
+    routes.map(item => {
+      return (
+        !item.component ? 
+          item.children?.length ? renderRoute(item.children) : '' :
+          (
+            <Route
+              {...item}
+              key={item.path}
+              path={item.path}
+              element={
+                renderElement(item)
+              }
+            >
+              {/* 嵌套路由 */}
+              {item.children?.length ? renderRoute(item.children) : ''}
+            </Route>
+          )
+        )
+    })
+  )
+}
+
+function RouteView ( {UserInfo}: any) {
 
   const [loading, setLoading] = useAsyncState(true)
 
@@ -21,8 +58,9 @@ function RouteView ( {appStore}: any) {
     setLoading(true)
     const res = await getUserInfo()
     if (res.code === 1) {
-      console.log(res)
-      appStore.toggleLogin({...res.data}, getToken())
+      UserInfo.update({...res.data})
+    } else {
+      UserInfo.update({uuid: 'root', username: 'root'})
     }
     setLoading(false)
     // eslint-disable-next-line
@@ -33,64 +71,26 @@ function RouteView ( {appStore}: any) {
     const token = getToken()
     if (!token) {
       setLoading(false)
-      return appStore.toggleLogin({})
+      return UserInfo.reset()
     }
     const userInfo = getToken('user-info') && JSON.parse(getToken('user-info'))
     if (userInfo.uuid) {
-      appStore.toggleLogin(userInfo, token)
+      UserInfo.update({...userInfo, token})
       setLoading(false)
     }
     if (!userInfo.uuid) _getUserInfo()
     // eslint-disable-next-line
-  }, [appStore.userInfo.uuid])
-
-  // 渲染路由对应的组件
-  const renderElement = (route:RouteInterface):any => {
-    return (
-      // 不支持渲染的，则跳转到对应配置的路径或404
-      !route?.meta?.noRender ?
-      (
-        <Suspense fallback={<LazyLoading />}>
-          <route.component {...route} />
-        </Suspense>
-      ) : <Navigate to={route?.to || '/404'} />
-    )
-  }
-
-  // 只渲染对应的Route,外层需要用Routes包裹
-  const renderRoute:any = (routes:Array<RouteInterface>) => {
-    return (
-      routes.map(item => {
-        return (
-          !item.component ? 
-            item.children?.length ? renderRoute(item.children) : '' :
-            (
-              <Route
-                {...item}
-                key={item.path}
-                path={item.path}
-                element={
-                  renderElement(item)
-                }
-              >
-                {/* 嵌套路由 */}
-                {item.children?.length ? renderRoute(item.children) : ''}
-              </Route>
-            )
-          )
-      })
-    )
-  }
+  }, [UserInfo.user.uuid])
 
   return (
     loading ? <LazyLoading /> :
     (
       <Routes>
-        { renderRoute(generateRoute(routers, !!appStore.userInfo?.uuid)) }
+        { renderRoute(generateRoute(routers, !!UserInfo.user?.token)) }
         <Route path="*" element={<Navigate to="/404" />} />
       </Routes>
     )
   )
 }
 
-export default inject("appStore")(observer(RouteView));
+export default inject("UserInfo")(observer(RouteView));
