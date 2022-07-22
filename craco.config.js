c6283781cc0ee048a2d3ab4a3e397ebe02ee852c
陈凯旋 <chenkaixuan@doctortech.com.cn>
2022-07-22 15:39:53 +0800

    添加项目打包优化

--- craco.config.js
+++ craco.config.js
@@ -2,6 +2,8 @@ const path = require('path')
 
 const pathResolve = pathUrl => path.join(__dirname, pathUrl)
 
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+
 const getProxy = (path = '') => {
   return {
     target: path || 'http://localhost:1010',
@@ -16,7 +18,44 @@ module.exports = {
   webpack: {
     alias: {
       '@': pathResolve('src'), // 配置@ （同时还要配置tsconfig，如下）
-    }
+    },
+    /** [分离第三方包](https://zhuanlan.zhihu.com/p/152097785) */
+    configure: (webpackConfig, { env, paths }) =>{
+      webpackConfig.devtool = false;
+      webpackConfig.optimization= {
+        splitChunks: {
+          minSize: 30000,
+          maxSize: 0,
+          minChunks: 1,
+          maxAsyncRequests: 6,
+          maxInitialRequests: 4,
+          automaticNameDelimiter: '~',
+          cacheGroups: {
+            vendors: {
+              name: `chunk-vendors`,
+              test: /[\\/]node_modules[\\/]/,
+              priority: -10,
+              chunks: 'all',
+            },
+            antd: {
+              name: `antd`,
+              test: /[\\/]antd[\\/]/,
+              priority: 0,
+              chunks: 'all'
+            },
+            components: {
+              name: `components`,
+              test: /[\\/]components[\\/]/,
+              priority: 0,
+            }
+          }
+        }
+      }
+      return webpackConfig
+    },
+    plugins:[
+      new BundleAnalyzerPlugin()
+    ],
   },
   babel: {
     plugins: [
