e14b2425ef27536f1a00713c549016205f425783
陈凯旋 <chenkaixuan@doctortech.com.cn>
2022-07-27 14:12:13 +0800

    调整深拷贝函数

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,45 +2,54 @@
  * 简单的深拷贝
  * @param {object} obj
  */
-const deepCopy = (obj: any): any => {
-  if (obj === null) return null;
-  if (typeof obj !== 'object') return obj;
-  let result: Array<any> | any = Array.isArray(obj) ? [] : {};
-  for (let key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      if (typeof obj[key] === 'object' && obj[key] !== null) {
-        result[key] = deepCopy(obj[key]);   //递归拷贝
-      } else {
-        result[key] = obj[key];
-      }
+const deepCopy = (target: any, map = new WeakMap()): any => {
+  if (typeof target === "object") {
+    const isArray: boolean = Array.isArray(target);
+    let cloneTarget: any = isArray ? [] : {};
+
+    // 防止循环引用
+    if (map.get(target)) {
+      return map.get(target);
     }
+    map.set(target, cloneTarget);
+
+    if (!isArray) {
+      Object.keys(target).forEach(key => {
+        cloneTarget[key] = deepCopy(target[key], map);
+      })
+    } else {
+      target.forEach((_: any, index: number) => {
+        cloneTarget[index] = deepCopy(target[index], map);
+      })
+    }
+
+    return cloneTarget;
+  } else {
+    return target;
   }
-  return result;
-}
+};
+
 
 /**
  * 防抖
  */
- function debounce1(this:any, fn: Function, wait: number) {
+function debounce1(this: any, fn: Function, wait: number) {
   //定义定时器变量
   let timer: NodeJS.Timeout | null = null;
   //保存当前函数this
   let context: void = this;
-  let args:IArguments = arguments;
+  let args: IArguments = arguments;
   return function () {
     //如果在wait时间内再次触发，清空定时器重新计算时间
-    if(timer) clearTimeout(timer)
+    if (timer) clearTimeout(timer);
     let callNow = !timer;
     //达到wait时间，将定时器指向null
     timer = setTimeout(() => {
       timer = null;
-    }, wait)
+    }, wait);
     //定时器为null时会执行回调函数，即fn，只要触发就一定会执行一次
-    if(callNow) fn.apply(context, args);
-  }
+    if (callNow) fn.apply(context, args);
+  };
 }
 
-export {
-  deepCopy,
-  debounce1
-}
+export { deepCopy, debounce1 };
